48401ea6f90d3b926d90a5ea9bd15abf9cd5002e
Ian <iansutherland.az@gmail.com>
2021-03-10 15:50:22 -0700

    initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Patagames.Ocr;
+using System.Drawing;
+using System.IO;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string picPath = @"Pic/Untitled.png";
+
+            if (File.Exists(picPath))
+            {
+                using(var ocr = OcrApi.Create())
+                {
+                    ocr.Init(Patagames.Ocr.Enums.Languages.English);
+
+                    string plainText = ocr.GetTextFromImage(picPath);
+
+                    Console.WriteLine(plainText);
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Couldn't find {picPath}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
